1745a050eeaab27d6898979f7e39301b026db131
Praveen Das <praveendas5294@gmail.com>
2020-01-22 04:01:27 +0530

    ok

---
+++ src/main/java/com/example/jibdemo/WebController.java
@@ -0,0 +1,26 @@
+package com.example.jibdemo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    @RequestMapping("/")
+    public String home() {
+
+        logger.info("home homehomehomehomehomehomehomehomehomehomehomehomehomehomehomehomehome");
+        // pre-java 8
+        if (logger.isDebugEnabled()) {
+            logger.debug("Hello from Log4j 2 - num ");
+        }
+
+        // java 8 lambda, no need to check log level
+        logger.debug("Hello from Log4j 2 - num sadsadsads:");
+
+        return "Welcome!";
+    }
+
+}
